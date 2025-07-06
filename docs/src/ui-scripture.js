c188fd6aab90d20976210491ee6faee212ddea77
tom <tom.broadbent@gmail.com>
2025-07-05 23:22:33 -0700

    tighten up scripture logic, layout

--- docs/src/ui-scripture.js
+++ docs/src/ui-scripture.js
@@ -3,29 +3,32 @@ window.ScriptureBlock = function ScriptureBlock({ items, onParagraphClick }) {
         <div className="bg-gray-100 border border-gray-300 rounded-2xl p-4 pt-2 shadow-sm">
             {items.map(({ scripture, citation, context, meaning }, index) => {
                 return (
-                    <div key={index} className={`${index !== 0 ? 'border-t border-gray-300 mt-2 py-2' : ''}`}>
+                    <div key={index}>
+                        {index !== 0 && (
+                            <div className="bg-green-20 border-t border-gray-300 mt-2 pb-2"></div>
+                        )}
                         <div className="text-lg font-bold text-gray-900 mb-1">
                             {window.parseScriptureLinks(scripture)}
                         </div>
                         {context && (
                             <div
-                                className="text-gray-800 mb-2 cursor-pointer"
+                                className="text-gray-800 mb-2"
                                 onClick={() => onParagraphClick({ type: 'text', content: context })}
                             >
                                 {context}
                             </div>
                         )}
-                        <div
-                            onClick={() => onParagraphClick({ type: 'text', content: [scripture, citation].join('\n') })}
-                            className="cursor-pointer"
-                        >
-                            <div className="bg-white p-2 rounded-lg shadow-sm text-lg text-gray-800 whitespace-pre-line">
+                        {citation && (
+                            <div
+                                className="bg-white p-2 rounded-lg shadow-sm text-lg text-gray-800 whitespace-pre-line"
+                                onClick={() => onParagraphClick({ type: 'text', content: [scripture, citation].join('\n') })}
+                            >
                                 {citation}
                             </div>
-                        </div>
+                        )}
                         {meaning && (
                             <div
-                                className="bg-blue-50 border border-blue-200 text-blue-900 mt-2 px-2 rounded cursor-pointer"
+                                className="bg-blue-50 border border-blue-200 text-blue-900 mt-2 px-2 rounded"
                                 onClick={() => onParagraphClick({ type: 'text', content: meaning })}
                             >
                                 {meaning}
